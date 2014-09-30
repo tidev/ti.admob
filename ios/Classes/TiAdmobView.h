97e7f6f05948d1388b56dbc4edb7e13d9cb66258
Jasper Kennis <contact@jasperkennis.nl>
2014-09-30 14:29:43 +0200

    Replace the originals with interstitials

--- ios/Classes/TiAdmobView.h
+++ ios/Classes/TiAdmobView.h
@@ -6,13 +6,34 @@
  */
 
 #import "TiUIView.h"
-#import "GADBannerView.h"
-#import "GADBannerViewDelegate.h"
+#import "GADInterstitial.h"
+#import "GADInterstitialDelegate.h"
 
-@interface TiAdmobView : TiUIView<GADBannerViewDelegate> {
-
+@interface TiAdmobView : TiUIView<GADInterstitialDelegate> {
+  
 @private
-	GADBannerView *ad;
+  GADInterstitial *interstitial;
 }
 
 @end
+
+//
+//
+///**
+// * Appcelerator Titanium Mobile
+// * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
+// * Licensed under the terms of the Apache Public License
+// * Please see the LICENSE included with this distribution for details.
+// */
+//
+//#import "TiUIView.h"
+//#import "GADBannerView.h"
+//#import "GADBannerViewDelegate.h"
+//
+//@interface TiAdmobView : TiUIView<GADBannerViewDelegate> {
+//
+//@private
+//	GADBannerView *ad;
+//}
+//
+//@end
--- ios/Classes/TiAdmobView.m
+++ ios/Classes/TiAdmobView.m
@@ -9,116 +9,146 @@
 #import "TiApp.h"
 #import "TiUtils.h"
 
-@implementation TiAdmobView
+@implementation TiAdmobInterstitialView
 
 #pragma mark -
 #pragma mark Ad Lifecycle
 
--(void)refreshAd:(CGRect)bounds
-{
-    if (ad != nil) {
-        [ad removeFromSuperview];
-        RELEASE_TO_NIL(ad);
-    }
-    
-    ad = [[GADBannerView alloc] initWithFrame:bounds];
-    
-    // Specify the ad's "unit identifier." This is your AdMob Publisher ID.
-    ad.adUnitID = [self.proxy valueForKey:@"adUnitId"];
-    
-    // Let the runtime know which UIViewController to restore after taking
-    // the user wherever the ad goes and add it to the view hierarchy.
-    ad.rootViewController = [[TiApp app] controller];
-    
-    // Initiate a generic request to load it with an ad.
-    GADRequest* request = [GADRequest request];
-    
-    if ([self.proxy valueForKey:@"publisherId"]) {
-        NSLog(@"`publisherId` has been removed. Use `adUnitId` instead.");
-    }
-    if ([TiUtils boolValue:[self.proxy valueForKey:@"testing"] def:NO]) {
-        NSLog(@"`testing` has been deprecated. Use `testDevices` instead.");
-        // testing is deprecated
-        request.testing = YES;
-    }
-    
-    // Go through the configurable properties, populating our request with their values (if they have been provided).
-    request.keywords = [self.proxy valueForKey:@"keywords"];
-    request.birthday = [self.proxy valueForKey:@"dateOfBirth"];
-    request.testDevices = [self.proxy valueForKey:@"testDevices"];
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  interstitial = [[GADInterstitial alloc] init];
+  interstitial.adUnitID = [self.proxy valueForKey:@"adUnitId"];
   
-    NSString* backgroundColor = [self.proxy valueForKey:@"adBackgroundColor"];
-    if (backgroundColor != nil) {
-        ad.backgroundColor = [[TiUtils colorValue:backgroundColor] _color];
-    }
-    
-    NSDictionary* location = [self.proxy valueForKey:@"location"];
-    if (location != nil) {
-        [request setLocationWithLatitude:[[location valueForKey:@"latitude"] floatValue]
-                               longitude:[[location valueForKey:@"longitude"] floatValue]
-                                accuracy:[[location valueForKey:@"accuracy"] floatValue]];
-    }
-    
-    NSString* gender = [self.proxy valueForKey:@"gender"];
-    if ([gender isEqualToString:@"male"]) {
-        request.gender = kGADGenderMale;
-    } else if ([gender isEqualToString:@"female"]) {
-        request.gender = kGADGenderFemale;
-    } else {
-        request.gender = kGADGenderUnknown;
-    }
-    
-    [self addSubview:ad];
-    ad.delegate = self;
-    [ad loadRequest:request];
+  GADRequest *request = [GADRequest request];
+  // Requests test ads on simulators.
+  request.testDevices = @[ GAD_SIMULATOR_ID ];
+  [interstitial loadRequest:request];
 }
-
--(void)frameSizeChanged:(CGRect)frame bounds:(CGRect)bounds
-{
-    [self refreshAd:bounds];
-}
-
--(void)dealloc
-{
-    if (ad != nil) {
-        [ad removeFromSuperview];
-        RELEASE_TO_NIL(ad);
-    }
-    [super dealloc];
-}
-
-#pragma mark -
-#pragma mark Ad Delegate
-
-- (void)adViewDidReceiveAd:(GADBannerView *)view
-{
-    [self.proxy fireEvent:@"didReceiveAd"];
-}
-
-- (void)adView:(GADBannerView *)view didFailToReceiveAdWithError:(GADRequestError *)error
-{
-    [self.proxy fireEvent:@"didFailToReceiveAd"];
-}
-
-- (void)adViewWillPresentScreen:(GADBannerView *)adView
-{
-    [self.proxy fireEvent:@"willPresentScreen"];
-}
-
-- (void)adViewWillDismissScreen:(GADBannerView *)adView
-{
-    [self.proxy fireEvent:@"willDismissScreen"];
-}
-
-- (void)adViewDidDismissScreen:(GADBannerView *)adView
-{
-    [self.proxy fireEvent:@"didDismissScreen"];
-}
-
-- (void)adViewWillLeaveApplication:(GADBannerView *)adView
-{
-    [self.proxy fireEvent:@"willLeaveApplication"];
-}
-
 
 @end
+
+
+///**
+// * Appcelerator Titanium Mobile
+// * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
+// * Licensed under the terms of the Apache Public License
+// * Please see the LICENSE included with this distribution for details.
+// */
+//
+//#import "TiAdmobView.h"
+//#import "TiApp.h"
+//#import "TiUtils.h"
+//
+//@implementation TiAdmobView
+//
+//#pragma mark -
+//#pragma mark Ad Lifecycle
+//
+//-(void)refreshAd:(CGRect)bounds
+//{
+//    if (ad != nil) {
+//        [ad removeFromSuperview];
+//        RELEASE_TO_NIL(ad);
+//    }
+//    
+//    ad = [[GADBannerView alloc] initWithFrame:bounds];
+//    
+//    // Specify the ad's "unit identifier." This is your AdMob Publisher ID.
+//    ad.adUnitID = [self.proxy valueForKey:@"adUnitId"];
+//    
+//    // Let the runtime know which UIViewController to restore after taking
+//    // the user wherever the ad goes and add it to the view hierarchy.
+//    ad.rootViewController = [[TiApp app] controller];
+//    
+//    // Initiate a generic request to load it with an ad.
+//    GADRequest* request = [GADRequest request];
+//    
+//    if ([self.proxy valueForKey:@"publisherId"]) {
+//        NSLog(@"`publisherId` has been removed. Use `adUnitId` instead.");
+//    }
+//    if ([TiUtils boolValue:[self.proxy valueForKey:@"testing"] def:NO]) {
+//        NSLog(@"`testing` has been deprecated. Use `testDevices` instead.");
+//        // testing is deprecated
+//        request.testing = YES;
+//    }
+//    
+//    // Go through the configurable properties, populating our request with their values (if they have been provided).
+//    request.keywords = [self.proxy valueForKey:@"keywords"];
+//    request.birthday = [self.proxy valueForKey:@"dateOfBirth"];
+//    request.testDevices = [self.proxy valueForKey:@"testDevices"];
+//  
+//    NSString* backgroundColor = [self.proxy valueForKey:@"adBackgroundColor"];
+//    if (backgroundColor != nil) {
+//        ad.backgroundColor = [[TiUtils colorValue:backgroundColor] _color];
+//    }
+//    
+//    NSDictionary* location = [self.proxy valueForKey:@"location"];
+//    if (location != nil) {
+//        [request setLocationWithLatitude:[[location valueForKey:@"latitude"] floatValue]
+//                               longitude:[[location valueForKey:@"longitude"] floatValue]
+//                                accuracy:[[location valueForKey:@"accuracy"] floatValue]];
+//    }
+//    
+//    NSString* gender = [self.proxy valueForKey:@"gender"];
+//    if ([gender isEqualToString:@"male"]) {
+//        request.gender = kGADGenderMale;
+//    } else if ([gender isEqualToString:@"female"]) {
+//        request.gender = kGADGenderFemale;
+//    } else {
+//        request.gender = kGADGenderUnknown;
+//    }
+//    
+//    [self addSubview:ad];
+//    ad.delegate = self;
+//    [ad loadRequest:request];
+//}
+//
+//-(void)frameSizeChanged:(CGRect)frame bounds:(CGRect)bounds
+//{
+//    [self refreshAd:bounds];
+//}
+//
+//-(void)dealloc
+//{
+//    if (ad != nil) {
+//        [ad removeFromSuperview];
+//        RELEASE_TO_NIL(ad);
+//    }
+//    [super dealloc];
+//}
+//
+//#pragma mark -
+//#pragma mark Ad Delegate
+//
+//- (void)adViewDidReceiveAd:(GADBannerView *)view
+//{
+//    [self.proxy fireEvent:@"didReceiveAd"];
+//}
+//
+//- (void)adView:(GADBannerView *)view didFailToReceiveAdWithError:(GADRequestError *)error
+//{
+//    [self.proxy fireEvent:@"didFailToReceiveAd"];
+//}
+//
+//- (void)adViewWillPresentScreen:(GADBannerView *)adView
+//{
+//    [self.proxy fireEvent:@"willPresentScreen"];
+//}
+//
+//- (void)adViewWillDismissScreen:(GADBannerView *)adView
+//{
+//    [self.proxy fireEvent:@"willDismissScreen"];
+//}
+//
+//- (void)adViewDidDismissScreen:(GADBannerView *)adView
+//{
+//    [self.proxy fireEvent:@"didDismissScreen"];
+//}
+//
+//- (void)adViewWillLeaveApplication:(GADBannerView *)adView
+//{
+//    [self.proxy fireEvent:@"willLeaveApplication"];
+//}
+//
+//
+//@end
