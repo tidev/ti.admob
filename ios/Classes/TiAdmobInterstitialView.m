7801e497078b1617b25581b0721be0005015c9d6
Jasper Kennis <contact@jasperkennis.nl>
2014-09-30 14:18:25 +0200

    Added interstitial view class

---
+++ ios/Classes/TiAdmobInterstitialView.m
@@ -0,0 +1,26 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2010-2013 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ */
+
+#import "TiAdmobInterstitialView.h"
+
+@implementation TiAdmobInterstitialView
+
+#pragma mark -
+#pragma mark Ad Lifecycle
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  interstitial = [[GADInterstitial alloc] init];
+  interstitial.adUnitID = [self.proxy valueForKey:@"adUnitId"];
+
+  GADRequest *request = [GADRequest request];
+  // Requests test ads on simulators.
+  request.testDevices = @[ GAD_SIMULATOR_ID ];
+  [interstitial loadRequest:request];
+}
+
+@end
