bdffa34037e71ee8dcb61aa7010cb90a0198f398
Jan Vennemann <jan.vennemann@gmx.net>
2020-05-12 12:47:58 +0200

    style(ios): fix formatting

--- ios/Classes/TiAdmobModuleAssets.h
+++ ios/Classes/TiAdmobModuleAssets.h
@@ -3,10 +3,9 @@
  */
 
 @interface TiAdmobModuleAssets : NSObject {
-
 }
 
 - (NSData *)moduleAsset;
-- (NSData *)resolveModuleAsset:(NSString*)path;
+- (NSData *)resolveModuleAsset:(NSString *)path;
 
 @end
