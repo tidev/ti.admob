c75befaeb371767cffeb722fe0660de15ccdadc4
hansemannn <h.knoechel@lambus.com>
2021-04-24 14:47:25 +0200

    chore: update example

--- ios/example/app.js
+++ ios/example/app.js
@@ -1,22 +1,19 @@
 var Admob = require('ti.admob');
+
 var win = Ti.UI.createWindow({
     backgroundColor: 'white',
     orientationModes: [Ti.UI.PORTRAIT, Ti.UI.LANDSCAPE_LEFT, Ti.UI.LANDSCAPE_RIGHT]
 });
 
-/*
- We'll make two ads. This first one doesn't care about where the user is located.
- */
-var ad1 = Admob.createView({
+/* Banner ads */
+
+var bannerAdView = Admob.createView({
     height: 50,
-    bottom: 0,
+    bottom: 30,
     debugEnabled: true, // If enabled, a dummy value for `adUnitId` will be used to test
     adType: Admob.AD_TYPE_BANNER,
     adUnitId: '<<YOUR ADD UNIT ID HERE>>', // You can get your own at http: //www.admob.com/
-    adBackgroundColor: 'black',
-    testDevices: [Admob.SIMULATOR_ID], // You can get your device's id by looking in the console log
-    dateOfBirth: new Date(1985, 10, 1, 12, 1, 1),
-    gender: Admob.GENDER_MALE, // GENDER_MALE, GENDER_FEMALE or GENDER_UNKNOWN, default: GENDER_UNKNOWN
+    adBackgroundColor: 'yellow',
     contentURL: 'https://admob.com', // URL string for a webpage whose content matches the app content.
     requestAgent: 'Titanium Mobile App', // String that identifies the ad request's origin.
     extras: {
@@ -26,49 +23,44 @@ var ad1 = Admob.createView({
     tagForChildDirectedTreatment: false, // http:///business.ftc.gov/privacy-and-security/childrens-privacy for more infos
     keywords: ['keyword1', 'keyword2']
 });
-win.add(ad1);
+win.add(bannerAdView);
 
-ad1.addEventListener('didReceiveAd', function(e) {
+bannerAdView.addEventListener('didRecordImpression', function(e) {
+    Ti.API.info('Ad impression recorded!');
+});
+bannerAdView.addEventListener('didReceiveAd', function(e) {
     Ti.API.info('Did receive ad: ' + e.adUnitId + '!');
 });
-ad1.addEventListener('didFailToReceiveAd', function(e) {
+bannerAdView.addEventListener('didFailToReceiveAd', function(e) {
     Ti.API.error('Failed to receive ad: ' + e.error);
 });
-ad1.addEventListener('willPresentScreen', function() {
+bannerAdView.addEventListener('didPresentScreen', function() {
     Ti.API.info('Presenting screen!');
 });
-ad1.addEventListener('willDismissScreen', function() {
+bannerAdView.addEventListener('willDismissScreen', function() {
     Ti.API.info('Dismissing screen!');
 });
-ad1.addEventListener('didDismissScreen', function() {
+bannerAdView.addEventListener('didDismissScreen', function() {
     Ti.API.info('Dismissed screen!');
 });
-ad1.addEventListener('willLeaveApplication', function() {
-    Ti.API.info('Leaving the app!');
-});
-ad1.addEventListener('didReceiveInAppPurchase', function(e) {
-    Ti.API.info('Did receive an In-App purchase: ' + e.productId + '!');
-    Ti.API.info(e);
+
+/* Interstitial Ads */
+
+var interstitialAdButton = Ti.UI.createButton({
+    title: 'Show interstitial ad'
 });
 
-var btn = Ti.UI.createButton({
-    title: 'Show interstitial'
-});
-
-btn.addEventListener('click', function() {
+interstitialAdButton.addEventListener('click', function() {
     var ad2 = Admob.createView({
         debugEnabled: true, // If enabled, a dummy value for `adUnitId` will be used to test
         adType: Admob.AD_TYPE_INTERSTITIAL,
         adUnitId: '<<YOUR ADD UNIT ID HERE>>', // You can get your own at http: //www.admob.com/
-        testDevices: [Admob.SIMULATOR_ID], // You can get your device's id by looking in the console log
-        dateOfBirth: new Date(1985, 10, 1, 12, 1, 1),
-        gender: Admob.GENDER_MALE, // GENDER_MALE or GENDER_FEMALE, default: undefined
         keywords: ['keyword1', 'keyword2']
     });
     ad2.receive();
 
-    ad2.addEventListener('didReceiveAd', function(e) {
-        Ti.API.info('Did receive ad!');
+    ad2.addEventListener('didRecordImpression', function(e) {
+        Ti.API.info('Ad impression recorded!');
     });
 
     ad2.addEventListener('didFailToReceiveAd', function(e) {
@@ -76,7 +68,9 @@ btn.addEventListener('click', function() {
     });
 });
 
-win.add(btn);
+win.add(interstitialAdButton);
+
+/* Rewarded Video Ads */
 
 var rewardedVideoButton = Ti.UI.createButton({
     title: 'Show Rewarded Video Ad',
@@ -84,6 +78,7 @@ var rewardedVideoButton = Ti.UI.createButton({
         y: '55%'
     }
 });
+
 rewardedVideoButton.addEventListener('click', function() {
     rewardedVideoButton.title = 'Loading Rewarded Video ...';
 
@@ -92,16 +87,12 @@ rewardedVideoButton.addEventListener('click', function() {
         adType: Admob.AD_TYPE_REWARDED_VIDEO,
     });
     rewardedVideo.receive();
-    rewardedVideo.addEventListener('adloaded', function() {
-        rewardedVideo.showRewardedVideo();
-    });
-    rewardedVideo.addEventListener('adrewarded', function (reward) {
+
+    rewardedVideo.addEventListener('didRecordImpression', function (reward) {
         Ti.API.debug(`Received reward! type: ${reward.type}, amount: ${reward.amount}`);
-        // pre load next rewarded video
-        // rewardedVideo.loadRewardedVideo('ad-unit-id');
     });
-    rewardedVideo.addEventListener('adclosed', function() {
-        Ti.API.debug('No gold for you!');
+    rewardedVideo.addEventListener('didDismissScreen', function() {
+        Ti.API.debug('Ad closed!');
     });
     rewardedVideo.addEventListener('adfailedtoload', function(error) {
         Ti.API.debug('Rewarded video ad failed to load: ' + error.message);
@@ -116,4 +107,5 @@ win.add(Ti.UI.createLabel({
     top: 40,
     textAlign: 'center'
 }));
+
 win.open();
